2bde96d88653404eca8283342423203c107326a2
DinhLeXuan <lexuandinh.ip@gmail.com>
2018-06-23 13:13:47 +0700

    -- Fix level design

--- assets/Script/SCR_Config.js
+++ assets/Script/SCR_Config.js
@@ -1,4 +1,4 @@
 window.OBSTACLE_MOVE_SPEED = 460;
-window.OBSTACLE_DISTANCE_K = 0.82;           // distance between obstacles = OBSTACLE_DISTANCE_K * SCREEN_WIDTH
-window.OBSTACLE_SPACE_K = 0.5;              // space between top and bottom obstacles = OBSTACLE_SPACE_K * SCREEN_WIDTH
-window.OBSTACLE_CENTER_RANDOM_RANGE = 0.5;
+window.OBSTACLE_DISTANCE_K = 0.75;           // distance between obstacles = OBSTACLE_DISTANCE_K * SCREEN_WIDTH
+window.OBSTACLE_SPACE_K = 0.45;              // space between top and bottom obstacles = OBSTACLE_SPACE_K * SCREEN_WIDTH
+window.OBSTACLE_CENTER_RANDOM_RANGE = 0.6;
