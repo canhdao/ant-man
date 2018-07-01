a6b7d38f3400d2d2c6aae29d3dc491d8551ea2bf
DinhLeXuan <lexuandinh.ip@gmail.com>
2018-07-01 18:02:59 +0700

    -- Balance level power up giant

--- assets/Script/SCR_Config.js
+++ assets/Script/SCR_Config.js
@@ -5,7 +5,7 @@ window.OBSTACLE_DISTANCE_K = 0.75;           // distance between obstacles = OBS
 window.OBSTACLE_SPACE_K = 0.48;              // space between top and bottom obstacles = OBSTACLE_SPACE_K * SCREEN_WIDTH
 window.OBSTACLE_CENTER_RANDOM_RANGE = 0.55;
 
-window.POWER_UP_RATE = 0.5;
+window.POWER_UP_RATE = 0.1;
 window.POWER_UP_DURATION = 3;
 window.POWER_UP_ROTATION_SPEED = 100;
-window.POWER_UP_MOVE_SPEED_MULTIPLIER = 3;
+window.POWER_UP_MOVE_SPEED_MULTIPLIER = 2;
