1ca53cf10370f59ba5ffe0ca4ca180fb045c838b
DinhLeXuan <lexuandinh.ip@gmail.com>
2018-07-05 13:41:54 +0700

    -- Balance new level design - spaces and speed of power up

--- assets/Script/SCR_Config.js
+++ assets/Script/SCR_Config.js
@@ -2,13 +2,13 @@ window.BACKGROUND_MOVE_SPEED = 50;
 
 window.OBSTACLE_MOVE_SPEED = 460;
 window.OBSTACLE_DISTANCE_K = 0.75;           // distance between obstacles = OBSTACLE_DISTANCE_K * SCREEN_WIDTH
-window.OBSTACLE_SPACE_K = 0.48;              // space between top and bottom obstacles = OBSTACLE_SPACE_K * SCREEN_WIDTH
+window.OBSTACLE_SPACE_K = 0.40;              // space between top and bottom obstacles = OBSTACLE_SPACE_K * SCREEN_WIDTH
 window.OBSTACLE_CENTER_RANDOM_RANGE = 0.55;
 
 window.POWER_UP_RATE = 0.1;
 window.POWER_UP_DURATION = 3;
 window.POWER_UP_ROTATION_SPEED = 200;
-window.POWER_UP_MOVE_SPEED_MULTIPLIER = 2;
+window.POWER_UP_MOVE_SPEED_MULTIPLIER = 3;
 
 window.POWER_UP_TRUCK_RATE = 1;
 
